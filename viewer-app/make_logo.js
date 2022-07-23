c90f738e233aead256c7c5f93bdda262bfdba9a4
Ray Deck <ray@raydeck.com>
2022-07-23 09:18:11 -0400

    feat: new logos from Drew in viewer

--- viewer-app/make_logo.js
+++ viewer-app/make_logo.js
@@ -2,13 +2,19 @@ const { execSync } = require("child_process");
 
 const sourceFile = process.argv[2];
 execSync(
-  `convert -background transparent "${sourceFile}" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 public/favicon.ico`,
+  `convert "${sourceFile}" -resize x192 -gravity center -extent 192x192 -background transparent public/logo192.png`,
+  {
+    stdio: "inherit",
+  }
+);
+execSync(
+  `convert "${sourceFile}" -resize x512 -gravity center -extent 512x512 -background transparent public/logo512.png`,
+  {
+    stdio: "inherit",
+  }
+);
+execSync(
+  `convert -background transparent public/logo512.png -define icon:auto-resize=16,24,32,48,64,72,96,128,256 public/favicon.ico`,
   { stdio: "inherit" }
 );
-execSync(`convert "${sourceFile}" -scale 192x192 public/logo192.png`, {
-  stdio: "inherit",
-});
-execSync(`convert "${sourceFile}" -scale 512x512 public/logo512.png`, {
-  stdio: "inherit",
-});
 execSync(`cp "${sourceFile}" src/logo.png`, { stdio: "inherit" });
