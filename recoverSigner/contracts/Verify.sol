a8804d8200ec5b8d30b859b8344ac793d4e63055
Ray Deck <ray@raydeck.com>
2022-07-31 19:53:42 -0400

    fix: boom goes the dynamite

--- recoverSigner/contracts/Verify.sol
+++ recoverSigner/contracts/Verify.sol
@@ -15,29 +15,4 @@ contract Verify {
         signer = ECDSA.recover(hash, sig);
         require(signer == _signer);
     }
-
-    function splitSignature(bytes memory sig)
-        public
-        pure
-        returns (
-            uint8,
-            bytes32,
-            bytes32
-        )
-    {
-        require(sig.length == 65);
-
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-        assembly {
-            // first 32 bytes, after the length prefix
-            r := mload(add(sig, 32))
-            // second 32 bytes
-            s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(sig, 96)))
-        }
-        return (v, r, s);
-    }
 }
