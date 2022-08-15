1a7df69b0cf369901d8fd1141d474f424eb96d95
Drew <drewclementsdesign@gmail.com>
2022-08-15 09:48:28 -0400

    Drew: UI Updates

--- viewer-gasless/src/ContractFinder.tsx
+++ viewer-gasless/src/ContractFinder.tsx
@@ -5,6 +5,7 @@ import Title from "./Title";
 import Topography from "./topography.svg";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { ethers } from "ethers";
+import Logo from "./logo.svg";
 const supportedChains = [
   { chainId: 137, name: "Polygon Mainnet" },
   { chainId: 80001, name: "Polygon Mumbai Testnet" },
@@ -15,9 +16,13 @@ const ContractFinder: FC = () => {
       className="h-screen w-full flex flex-col justify-center items-center"
       style={{ background: `url(${Topography})` }}
     >
-      <div className="flex flex-col space-y-8">
-        <Title />
-        <h1 className="text-xl font-bold">Find a Smart Contract</h1>
+      <div className="flex flex-col space-y-8 items-center p-4 lg:p-0">
+        <div className="flex items-center space-x-6">
+          <img src={Logo} alt="Logo" className="w-12 lg:w-20" />
+          <h1 className="text-4xl lg:text-7xl font-bold text-purple-default">
+            PolyDocs
+          </h1>
+        </div>
         <div className="flex flex-row justify-center">
           <Formik
             initialValues={{
@@ -53,75 +58,79 @@ const ContractFinder: FC = () => {
           >
             {({ values, isValid, isSubmitting }) => (
               <Form>
-                <div>
-                  <label className="text-base font-medium text-gray-900">
-                    Blockchain
-                  </label>
-                  <p className="text-sm leading-5 text-gray-500">
-                    Which chain is the smart contract on?
-                  </p>
-
-                  <Field as="fieldset" name="chainID" className="mt-4">
-                    <legend className="sr-only">Blockchain</legend>
-                    <div className="space-y-4">
-                      {supportedChains.map(({ chainId, name }) => (
-                        <div key={chainId} className="flex items-center">
-                          <Field
-                            id={"chain_" + chainId}
-                            name="chainID"
-                            type="radio"
-                            checked={chainId == values.chainID}
-                            onChange={() => {
-                              console.log("I changed", chainId);
-                            }}
-                            value={chainId}
-                            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                          />
-                          <label
-                            htmlFor={"chain_" + chainId}
-                            className="ml-3 block text-sm font-medium text-gray-700"
-                          >
-                            {name}
-                          </label>
-                        </div>
-                      ))}
+                <div className="bg-white p-6 lg:p-10 doc-shadow">
+                  <h1 className="text-xl font-bold mb-6">
+                    Find a Smart Contract
+                  </h1>
+                  <div className="grid lg:grid-cols-2 gap-2 lg:gap-6 mb-10 lg:mb-6">
+                    <div>
+                      <label className="text-base font-medium text-gray-900">
+                        Blockchain
+                      </label>
+                      <p className="text-xs leading-5 opacity-75">
+                        Which chain is the smart contract on?
+                      </p>
                     </div>
-                  </Field>
-                </div>
-                <div className="mt-4">
-                  <label
-                    htmlFor="contractAddress"
-                    className="block text-base font-medium text-gray-900"
-                  >
-                    Contract Address
-                  </label>
-                  <p className="text-sm leading-5 text-gray-500">
-                    What is the address (starting with 0x followed by 40
-                    characters) of the smart contract?
-                  </p>
-                  <div className="mt-1">
-                    <Field
-                      type="text"
+
+                    <Field as="fieldset" name="chainID" className="mt-4">
+                      <legend className="sr-only">Blockchain</legend>
+                      <div className="space-y-4">
+                        {supportedChains.map(({ chainId, name }) => (
+                          <div key={chainId} className="flex items-center">
+                            <Field
+                              id={"chain_" + chainId}
+                              name="chainID"
+                              type="radio"
+                              checked={chainId == values.chainID}
+                              onChange={() => {
+                                console.log("I changed", chainId);
+                              }}
+                              value={chainId}
+                              className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+                            />
+                            <label
+                              htmlFor={"chain_" + chainId}
+                              className="ml-3 block text-sm font-medium text-gray-700"
+                            >
+                              {name}
+                            </label>
+                          </div>
+                        ))}
+                      </div>
+                    </Field>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="contractAddress"
+                      className="block text-base font-medium text-gray-900"
+                    >
+                      Contract Address
+                    </label>
+                    <p className="text-xs leading-5 opacity-75">
+                      What is the address (starting with 0x followed by 40
+                      characters) of the smart contract?
+                    </p>
+                    <div className="mt-4">
+                      <Field
+                        type="text"
+                        name="contractAddress"
+                        id="contractAddress"
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md w-100"
+                        placeholder="0x0000000000000000000000000000000000000000"
+                      />
+                    </div>
+                    <ErrorMessage
+                      component="div"
                       name="contractAddress"
-                      id="contractAddress"
-                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md w-100"
-                      placeholder="0x0000000000000000000000000000000000000000"
+                      className="text-red-500 font-xs"
                     />
                   </div>
-                  <ErrorMessage
-                    component="div"
-                    name="contractAddress"
-                    className="text-red-500 font-xs"
-                  />
                 </div>
-                <div className="mt-4">
+                <div className="mt-4 float-right">
                   <button
                     disabled={!isValid || isSubmitting}
                     type="submit"
-                    className={[
-                      "btn",
-                      isValid && !isSubmitting ? "btn-primary" : "btn-disabled",
-                    ].join(" ")}
+                    className="btn btn-primary"
                   >
                     Find Contract
                   </button>
