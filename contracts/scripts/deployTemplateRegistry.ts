a9e5a1481f5d79355087b562fffd8bd15ef82673
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-08-15 22:07:58 -0400

    feat: adding deploy script

--- contracts/scripts/deployTemplateRegistry.ts
+++ contracts/scripts/deployTemplateRegistry.ts
@@ -1,8 +1,6 @@
 import hre, { network, ethers } from "hardhat";
-import fs from "fs";
-import { execSync } from "child_process";
 async function main() {
-  // const [signer_1, signer_2, signer_3] = await ethers.getSigners();
+  const [signer_1, signer_2, signer_3] = await ethers.getSigners();
 
   const Doc = await ethers.getContractFactory("TemplateRegistry");
   const doc = await Doc.deploy();
@@ -10,27 +8,20 @@ async function main() {
 
   console.log("TemplateRegistry deployed to:", doc.address);
 
-  let config = `
-  export const TemplateRegistry address = "${doc.address}";
-  `;
-  const onThisNetwork = network.name;
-  if (onThisNetwork && onThisNetwork !== "hardhat") {
-    setTimeout(() => {
-      const cmd = `yarn hardhat verify ${doc.address} --network ${onThisNetwork}`;
-      console.log("Running", cmd);
-      execSync(cmd, {
-        stdio: "inherit",
-      });
-    }, 30000);
-  }
-  let data = JSON.stringify(config);
-  fs.writeFileSync("./config.ts", JSON.parse(data));
-  fs.writeFileSync(
-    "contracts.txt",
-    `${new Date().toLocaleString()}: ${network.config.chainId}: ${doc.address}
-`,
-    { flag: "a" }
-  );
+
+  const addTemplate_1 = await doc.add({"name":"name_1","cid":"randomcid_1", "score":0, "MetadataURI":"MetadataURI_1", "owner":signer_1.address}); 
+  const addTemplateReceipt_1 = await addTemplate_1.wait();
+  console.log("Added template 1 !");
+
+  const addTemplate_2 = await doc.add("randomcid_2")
+  const addTemplateReceipt_2 = await addTemplate_2.wait();
+  console.log("Added template 2!");
+
+  const addTemplate_3 = await doc.add("randomcid_3")
+  const addTemplateReceipt_3 = await addTemplate_3.wait();
+  console.log("Added template 3!");
+
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
