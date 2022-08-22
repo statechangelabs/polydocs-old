2a1dec5b748da40d99da19b5ab3c415e8c0c955c
Drew <drewclementsdesign@gmail.com>
2022-08-22 09:17:49 -0400

    drew: update contract editor

--- composer-app/src/Contract.tsx
+++ composer-app/src/Contract.tsx
@@ -23,6 +23,13 @@ import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useKnownTemplates } from "./useKnownTemplates";
 import { useProvider } from "./provider";
+import {
+  ErrorMessage as FormikErrorMessage,
+  Field,
+  Form,
+  Formik,
+} from "formik";
+import { DropFile } from "./DropFile";
 const knownRenderers = [
   "bafybeig44fabnqp66umyilergxl6bzwno3ntill3yo2gtzzmyhochbchhy",
 ];
@@ -161,7 +168,7 @@ const ContractDocument: FC = () => {
   // const knownTemplates = useIPFSList(knownCids);
   return (
     <Fragment>
-      <hr className="my-4" />
+      <hr className="my-6 border-gray-300" />
       <h2 className="text-xl font-bold text-gray-700">Terms Document</h2>
       <div>
         <div className="flex space-x-6 items-center justify-end mb-12">
@@ -173,14 +180,6 @@ const ContractDocument: FC = () => {
               setContractAddress(e.target.value);
             }} 
           /> */}
-          <a
-            className="btn btn-primary text-center"
-            href={`https://sign.polydocs.xyz/#/redirect::${chainId}::${contractAddress}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open Signing Page in New Tab
-          </a>
         </div>
         <div className="grid grid-cols-2 gap-x-6">
           <div>
@@ -252,7 +251,7 @@ const ContractDocument: FC = () => {
                     )}
                   </div>
                   <button
-                    className="btn btn-primary mt-6"
+                    className="btn btn-primary mt-6 text-xs"
                     onClick={() => setShowTemplateForm(true)}
                   >
                     Change
@@ -309,7 +308,7 @@ const ContractDocument: FC = () => {
                     <h3 className="font-semibold mb-2">Known Templates</h3>
                     <ul>
                       {knownTemplates.map(({ cid, name }) => (
-                        <li className="flex justify-between mb-6 ">
+                        <li className="flex justify-between gap-4 mb-6 ">
                           <button
                             onClick={() => {
                               setCurrentTemplate(cid);
@@ -340,12 +339,12 @@ const ContractDocument: FC = () => {
                         <h3 className="font-semibold mb-2">My Templates</h3>
                         <ul>
                           {Object.entries(templates).map(([cid, template]) => (
-                            <li className="flex justify-between  mb-4 ">
+                            <li className="flex justify-between gap-4 mb-4 ">
                               <button
                                 onClick={() => {
                                   setCurrentTemplate(cid);
                                 }}
-                                className="text-gray-60 text-primary-default hover:text-primary-light truncate"
+                                className="text-left gap-1text-gray-60 text-primary-default hover:text-primary-light truncate"
                               >
                                 <div>
                                   {template
@@ -453,6 +452,16 @@ const ContractDocument: FC = () => {
   );
 };
 
+const ErrorMessage: FC<{ name: string }> = ({ name }) => {
+  return (
+    <FormikErrorMessage
+      component="div"
+      name={name}
+      className="text-red-500 text-xs pt-2"
+    />
+  );
+};
+
 const ContractEditor: FC = () => {
   const { setTitle } = useMain();
   useEffect(() => {
@@ -464,6 +473,7 @@ const ContractEditor: FC = () => {
   const provider = useProvider(chainId);
   const [name, setName] = useState("");
   const [symbol, setSymbol] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
   useAsyncEffect(async () => {
     const contract = ERC721Termsable__factory.connect(
       contractAddress,
@@ -476,9 +486,109 @@ const ContractEditor: FC = () => {
     const uri = await contract.URI();
     const json = await getIPFSText(uri);
     const obj = JSON.parse(json);
-    const image = JSON.parse(image);
-    const;
+    // const image = JSON.parse(image);
   }, [provider]);
+
+  return (
+    <>
+      <Formik
+        initialValues={{ title: "", description: "", thumbnail: "", cover: "" }}
+        onSubmit={async (values) => {
+          console.log("submit");
+        }}
+      >
+        <div>
+          <div className="container-narrow mb-12">
+            <div className="bg-white doc-shadow p-6">
+              <div className="pb-6 border-b border-gray-100">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                  Profile
+                </h3>
+                <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                  General information about the contract
+                </p>
+              </div>
+              <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:py-6">
+                <label
+                  htmlFor="title"
+                  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Short Description
+                </label>
+                <div className="mt-1 sm:mt-0 sm:col-span-2">
+                  <div className="max-w-lg flex  shadow-sm">
+                    {/* <span className="inline-flex items-center px-3 -l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
+                        workcation.com/
+                      </span> */}
+                    <Field
+                      type="text"
+                      name="title"
+                      id="title"
+                      autoComplete="title"
+                      className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0  sm:text-sm border-gray-300"
+                    />
+                  </div>
+                  <ErrorMessage name="title" />
+                </div>
+              </div>
+
+              <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-100 sm:py-6">
+                <label
+                  htmlFor="description"
+                  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Longer Description <span className="text-red-500">*</span>
+                  <p className="mt-2 text-xs opacity-50">
+                    A description that will go into each token. The link to sign
+                    the contract terms will be substituted where you add
+                    [POLYDOCS]
+                  </p>
+                </label>
+                <div className="mt-1 sm:mt-0 sm:col-span-2">
+                  <Field
+                    component="textarea"
+                    id="description"
+                    name="description"
+                    rows={3}
+                    className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 "
+                    defaultValue={
+                      "Purchasing this token requires accepting our service terms: [POLYDOCS]"
+                    }
+                  />
+                  <ErrorMessage name="description" />
+                </div>
+              </div>
+
+              <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center sm:border-t sm:border-gray-100 sm:py-6">
+                <label
+                  htmlFor="thumbnail"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Thumbnail/logo image for the contract
+                </label>
+                <div className="mt-1 sm:mt-0 sm:col-span-2">
+                  <div className="flex items-center">
+                    <DropFile name="thumbnail" onUploading={setIsUploading} />
+                  </div>
+                </div>
+              </div>
+
+              <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-100 sm:pt-5">
+                <label
+                  htmlFor="cover-photo"
+                  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Cover Image
+                </label>
+                <DropFile name="cover" onUploading={setIsUploading} />
+              </div>
+            </div>
+            <button className="btn btn-primary mt-6">Save Changes</button>
+          </div>
+        </div>
+      </Formik>
+    </>
+  );
 };
 
 const Contract: FC = () => {
@@ -505,15 +615,40 @@ const Contract: FC = () => {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold text-primary">
-        {name} ({symbol}){" "}
-        {chainId === "80001" && (
-          <span className=" text-yellow-dark bg-yellow-100 px-2 mt-0.5 rounded-full text-[10px]">
-            Testnet
-          </span>
-        )}
-      </h1>
-      <div className="text-xs">{contractAddress}</div>
+      <div className="mb-6">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-2xl font-bold text-primary">
+              {name} ({symbol}){" "}
+              {chainId === "80001" && (
+                <span className=" text-yellow-dark bg-yellow-100 px-2 mt-0.5 rounded-full text-[10px]">
+                  Testnet
+                </span>
+              )}
+            </h1>
+            <p className="text-xs">{contractAddress}</p>
+          </div>
+          <div>
+            <a
+              className="btn-gradient btn mr-2"
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              View on Block Explorer
+            </a>
+            <a
+              className="btn btn-primary text-center"
+              href={`https://sign.polydocs.xyz/#/redirect::${chainId}::${contractAddress}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open Signing Page in New Tab
+            </a>
+          </div>
+        </div>
+      </div>
+      <ContractEditor />
       <ContractDocument />
     </div>
   );
